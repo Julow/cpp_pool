e8117552fe1d01b428c02b2c4235ec77128c0bba
jaguillo <juloo.dsi@gmail.com>
2015-04-09 15:59:00 +0200

    ex02

---
+++ ex02/FragTrap.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/04/09 12:24:56 by jaguillo          #+#    #+#             */
+/*   Updated: 2015/04/09 15:52:06 by jaguillo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+# include <string>
+# include "ClapTrap.hpp"
+
+class	FragTrap : public ClapTrap
+{
+public:
+	FragTrap(std::string const &name);
+	FragTrap(FragTrap const &src);
+	virtual ~FragTrap(void);
+
+	void				vaulthunter_dot_exe(std::string const &target);
+
+	FragTrap			&operator=(FragTrap const &rhs);
+
+private:
+	FragTrap(void);
+};
+
+#endif
---
+++ ex02/ScavTrap.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/04/09 12:24:56 by jaguillo          #+#    #+#             */
+/*   Updated: 2015/04/09 15:50:34 by jaguillo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include <string>
+# include "ClapTrap.hpp"
+
+class	ScavTrap : public ClapTrap
+{
+public:
+	ScavTrap(std::string const &name);
+	ScavTrap(ScavTrap const &src);
+	virtual ~ScavTrap(void);
+
+	void				challengeNewcomer(void);
+
+	ScavTrap			&operator=(ScavTrap const &rhs);
+
+private:
+	ScavTrap(void);
+};
+
+#endif
