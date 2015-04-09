63f11ae6d509d525421450a682fc6a94cc58aeca
jaguillo <juloo.dsi@gmail.com>
2015-04-09 15:38:12 +0200

    srand

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,16 +6,18 @@
 /*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/04/09 12:24:47 by jaguillo          #+#    #+#             */
-/*   Updated: 2015/04/09 15:15:52 by jaguillo         ###   ########.fr       */
+/*   Updated: 2015/04/09 15:37:54 by jaguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
+#include <cstdlib>
 
 int				main(void)
 {
 	FragTrap		frag("ROBO");
 
+	srand(time(NULL));
 	frag.rangedAttack("Lol");
 	frag.meleeAttack("Lol");
 	frag.takeAttack(8);
